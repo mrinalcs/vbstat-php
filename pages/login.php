09d8fd69bf72076f5d5221d79392296cb2d5d94a
Mrinal <mrinalvbu@gmail.com>
2023-09-22 08:39:25 +0530

    Create login.php

---
+++ pages/login.php
@@ -0,0 +1,19 @@
+<?php
+// privacy-policy.php
+
+$title = 'login';
+ $b_page=404;
+
+ 
+
+
+// Include the header
+require_once "$includes_folder/header.php";
+?>
+
+ 404
+
+<?php
+// Include the footer
+require_once "$includes_folder/footer.php";
+?>
