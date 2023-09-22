e49aceea6b75e62987e941a89ad3907f6f83c409
Mrinal <mrinalvbu@gmail.com>
2023-09-22 08:37:03 +0530

    Create about.php

---
+++ pages/about.php
@@ -0,0 +1,66 @@
+<?php
+
+$title = 'About';
+$description = 'The Department of Statistics at Siksha-Bhavana (Institute of Science) offers undergraduate, postgraduate, and PhD programs since 1999. Gain comprehensive statistical and computer training, and acquire skills in data analysis using advanced programming languages. Explore our interdisciplinary research initiatives led by our dedicated faculty members.';
+$keywords = 'about, statistics, department';
+// $theme_color='';
+
+
+//$og_title     /* if not declared default title will be used */
+// $og_description='Explore the Department of Statistics at VisvaBharati University and access lecture notes, gallery, and resources.';   /* if not declared default description will be used */
+// $og_image_url='https://vbstat.ml/img/2022/siksha-bhabana-football-tournament-2022.jpg';     /* declare this */
+$og_type='website';      /* declare this */
+
+
+
+
+
+// Include the header
+require_once "$includes_folder/header.php";
+?>
+<div class="max-w-screen-xl px-4 py-3 mx-auto md:px-6">
+
+    <h1 class="text-4xl font-extrabold dark:text-white">About</h1>
+
+
+    <p class="pt-4 text-gray-900 dark:text-white break-word">The Department started in 1999 with its Undergraduate Programmes in Statistics both at Major and Minor levels under Siksha-Bhavana (Institute of Science). It was augmented with a Post Graduate Programme in 2003 and a PhD Programme has also started side by side. It imparts rigorous training and exposure to the students in computer education by way of introducing the latest state-of-the-art in the programming language and computer software to enable the students to perform statistical data analysis. From inception, apart from its regular courses, it renders service in interdisciplinary research activity. The faculty members have been actively engaged in research in both basic and interdisciplinary fields.</p>
+
+    <figure>
+        <img class="h-auto max-w-lg mx-auto" src="<?php echo $url; ?>/img/p/dept-stat.jpg" alt="visvabharati stat dept" width="100%">
+        <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Dept of Statistics VisvaBhatai</figcaption>
+    </figure>
+
+    <br>
+
+    <h2 class="text-4xl font-extrabold dark:text-white">Santiniketan</h2>
+
+    <p class="pt-4 text-gray-900 dark:text-white break-word">To study the mind of man in its realisation of different aspects of truth from diverse points of view.
+
+        To bring into more intimate relation with one another, through patient study and research, the different cultures of the East on the basis of their underlying unity.
+
+        To approach the West from the standpoint of such a unity of the life and thought of Asia.
+
+        To seek to realize in a common fellowship of study the meeting of the East and the West, and thus ultimately to strengthen the fundamental conditions of world peace through the establishment of free communication of ideas between the two hemispheres.
+
+        And, with such ideals in view, to provide at Santiniketan, a centre of culture where research into and study of the religion, literature, history, science and art of Hindu, Buddhist, Jain, Islamic, Sikh, Christian and other civilisations may be pursued along with the culture of the West, with that simplicity in externals which is necessary for true spiritual realisation, in amity, good fellowship and co-operation between the thinkers and scholars of both Eastern and Western countries.</p>
+
+    <br>
+    <br class="pt-4 p-4">
+
+    <figure>
+        <img class="h-auto max-w-lg mx-auto" src="<?php echo $url; ?>/img/p/shantiniketan11.jpg" alt="visvabharati teaching" width="100%">
+        <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Santiniketan, VisvaBharati University</figcaption>
+    </figure>
+
+    <br>
+
+    <h2 class="text-4xl font-extrabold dark:text-white">Sriniketan</h2>
+    <p class="pt-4 text-gray-900 dark:text-white"> To win the friendship and affection of villagers and cultivators by taking a real interest in all that concerns their life and welfare, and by making an effort to assist them in solving their most pressing problems.
+
+        To initiate a dialogue between academic study and research of rural economy / culture and on-field experience.</p>
+
+</div>
+<?php
+// Include the header
+require_once "$includes_folder/footer.php";
+?>
