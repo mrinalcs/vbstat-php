2392d1fe7f043dfa054d33a81b190ef940a6fa60
Mrinal <mrinalvbu@gmail.com>
2023-09-22 08:41:58 +0530

    Create index.php

---
+++ smtp/index.php
@@ -0,0 +1,180 @@
+<?php
+
+$title = 'Projects on Statistical Analysis by Students';
+$description = 'Explore a collection of statistical analysis projects created by students. Gain insights into various statistical techniques, methodologies, and applications.';
+$keywords = 'statistical analysis, projects, students, data analysis, statistics';
+$author = 'Department of Statistics, VisvaBharati University'; 
+$theme_color='red';
+
+
+$og_title='BSc Final Year Statistical Analysis Projects';     /* if not declared default title will be used */
+$og_description='Explore a collection of statistical analysis projects created by BSc final year students. Gain insights into various statistical techniques, methodologies, and applications in their research papers.';   /* if not declared default description will be used */
+ $og_image_url='https://vbstat.ml/image/project-og.jpg';     /* declare this */
+$og_type='website';      /* declare this */
+
+
+
+
+
+// Include the header
+require_once "$includes_folder/header.php";
+?>
+<div class="project-ab max-w-screen-xl px-4 py-3 mx-auto md:px-6">
+<h1 class="  text-3xl text font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Project</span> 2022</h1>
+ <p class="text-lg text-gray-500 xl:mr-64 lg:mb-0 dark:text-gray-400 py-2"></p>
+
+<ul  class="bg-gray-100  text-sm font-medium     rounded-lg  dark:bg-gray-800 dark:text-white mt-8">
+    <li class=" px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1rdBseLxqt21Vy_Sj1fxAyrx_SF96L4E3/view?usp=drive_web" target="_blank"> 16-Titir.pptx </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1qbpSqzo4JuvfJIPtynK1ETFepeq1ynEQ/view?usp=drive_web" target="_blank"> A Statistical study of the Production and Export 123.pptx </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1roplLBqFIhXitORCUqxpXAkmXHaogcKj/view?usp=drive_web" target="_blank"> A STATISTICAL STUDY ON CENTRE WISE CIRCULATION OF NEWSPAPERS IN INDIA.pdf </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1rW4S1qPw0j61ervrvT2EXNtLm4OwKa3z/view?usp=drive_web" target="_blank"> B Sc(Sem-VI)Stat-02.pdf </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1rNDM5jtfzJS0ARC8piDdWe0Di8Uvgtv_/view?usp=drive_web" target="_blank"> B. Sc(sem-Vi)Stat-14.pdf </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1r0yOc-Y-c1iG0UqzsQhbDCgfXje62LKl/view?usp=drive_web" target="_blank"> B.Sc(Sem -V)Stat-01 ppt.pdf </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1rLcNNnZyTyr-Xuhirnsijo2Cf5j_6kYI/view?usp=drive_web" target="_blank"> B.Sc(sem-vi)-Stat-05-project_ppt.pdf </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1rHIwmZr4Bxxh5rakG4VL-0gbsS_LxCBD/view?usp=drive_web" target="_blank"> B.Sc(Sem-VI)Stat- 04 presentation.pdf </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1qYqMrBmQYIvyX054V85KnctxcbSpzHX1/view?usp=drive_web" target="_blank"> B.Sc(Sem-VI)Stat- 04 report.pdf </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1qlNtNlfkQ82x9TeWgJXR0zWmOLcyYrPu/view?usp=drive_web" target="_blank"> B.SC(Sem-VI)Stat-01report.pdf </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1rRv8gGb9nM2tgDTponQ3OY6AS5vPzsfQ/view?usp=drive_web" target="_blank"> B.Sc(Sem-VI)Stat-02report.pdf </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1rHWkg8LJmyAzINAwMJ0Uk_icWkrBGJS0/view?usp=drive_web" target="_blank"> B.Sc(sem-VI)stat-03.pdf </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1quJ_BuQx4cItBYdjNWovuc20a_UQ3QHg/view?usp=drive_web" target="_blank"> B.Sc(sem-vi)Stat-05-project_report.pdf </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1r3J3QpgrbkeScTB0V16mwbLM9iCGUR9a/view?usp=drive_web" target="_blank"> B.Sc(Sem-VI)Stat-06-ppt.pdf </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1qmu0SWEtaSr7-pRTDlPtIh0QV8hfukTl/view?usp=drive_web" target="_blank"> B.Sc(Sem-VI)Stat-06-Report.pdf </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://docs.google.com/document/d/1QZJp552PsMmA6DeVJ0yXaAoJji_OeLo8hfg0a5wW8JU/edit?usp=drive_web" target="_blank"> B.Sc(Sem-VI)Stat-07-ppt </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1qjej1ioyovmeH9g7Uxl-7SNgt4g95fCA/view?usp=drive_web" target="_blank"> B.Sc(Sem-VI)Stat-07-ppt.pdf </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1qpVjH_78C-g4lvMkH06tbXTpzENIksxQ/view?usp=drive_web" target="_blank"> B.Sc(Sem-VI)Stat-07-report.pdf </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1qsWexz48D39o6pF1yGDN4MMQ8smFShdc/view?usp=drive_web" target="_blank"> B.sc(sem-VI)stat-09_PPT.pdf </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1qzxft1ex15-qVNHPjZEcmQAx7jdYWb0E/view?usp=drive_web" target="_blank"> B.sc(sem-VI)stat-09_REPORT.pdf </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1r4BDoS5wWbFHEU0FylCM4ENdfXZ_zzCM/view?usp=drive_web" target="_blank"> B.Sc(Sem-VI)Stat-11.pdf </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1r36kT8v3jLUODm7HESrWQkQnb7iXP_ae/view?usp=drive_web" target="_blank"> B.Sc(Sem-VI)Stat-11.pptx </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1qqwCFjPtOLX9ADOzxMVIPqEdLwxQIEO7/view?usp=drive_web" target="_blank"> B.Sc(Sem-VI)Stat-17.pdf </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1rQ2R2xQfhp-67WbHPHB75irDDeo67KXT/view?usp=drive_web" target="_blank"> B.Sc.-(Hons.)-Statistics-Sem-VI-Stat-15.pptx </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1rBwbA1Gwk32D7HyL7PwUU1mVeggtFpyj/view?usp=drive_web" target="_blank"> Bsc(sem-VI)stat-03.docx </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1r5pFzQ6Eq22GoqKs-Ye0NNk3Jw6k1ZQX/view?usp=drive_web" target="_blank"> BSc(Sem-VI)Stat-17.pdf </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1rOJd7z_zUVqTl_mw-Y_E0pMl6jU3b2jC/view?usp=drive_web" target="_blank"> Heart_Disease_Prediction.pdf </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1riDBxdK5DyCYNzOBx3LiZyTRfIa8LvZz/view?usp=drive_web" target="_blank"> Presentation - INAMUL.pdf </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1rlj6-AN38hd-Y-Vc8u_wS1zJ_x54JnQU/view?usp=drive_web" target="_blank"> PRESENTATION - INAMUL.pptx </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1rQvS8bpHP3u7_kQGXxLEvXANH2mPRx4S/view?usp=drive_web" target="_blank"> REPORT (1).pdf </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1roiUnguwxeLQsf83A3WexG9uZxK918rX/view?usp=drive_web" target="_blank"> SAPTARSHI HANSDA BSC(SEM-VI)STAT-12.pptx </a>
+    </li>
+    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+      <a href="https://drive.google.com/file/d/1rRbEDg5Gu_O5XwoBHp2fh_eqR5hfZJn8/view?usp=drive_web" target="_blank"> Sarnalina-13-ppt.pdf </a>
+    </li>
+    <li class="w-full px-4 py-2 rounded-b-lg">
+      <a href="https://drive.google.com/file/d/1rOjZlPAALV_Y8qYIb84XIP1ovC2Evdgm/view?usp=drive_web" target="_blank"> Sarnalina-FINAL-PROJECT-SEM-VI.pdf </a>
+    </li>
+  </ul> 
+
+
+
+
+
+<h1 class="mb-4 text-3xl text font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl pt-8"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Project</span> 2023</h1>
+
+<div role="status" class="  p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700">
+    <div class="flex items-center justify-between">
+        <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+        </div>
+        <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+    </div>
+    <div class="flex items-center justify-between pt-4">
+        <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+        </div>
+        <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+    </div>
+    <div class="flex items-center justify-between pt-4">
+        <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+        </div>
+        <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+    </div>
+    <div class="flex items-center justify-between pt-4">
+        <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+        </div>
+        <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+    </div>
+    <div class="flex items-center justify-between pt-4">
+        <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+        </div>
+        <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+    </div>
+    <span class="sr-only">Loading...</span>
+</div>
+
+
+
+
+</div>
+<?php
+// Include the header
+require_once "$includes_folder/footer.php";
+?>
