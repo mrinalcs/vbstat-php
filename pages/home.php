8e2961157f1dbc26c235c96b4649d70f47c9ce68
Mrinal <mrinalvbu@gmail.com>
2023-09-22 08:38:54 +0530

    Create home.php

---
+++ pages/home.php
@@ -0,0 +1,170 @@
+<?php
+
+$title = 'Department of Statistics - VisvaBharati University';
+$description = 'Welcome to the unofficial website of the Department of Statistics at VisvaBharati University. Explore our lecture notes, gallery, and resources.';
+$keywords = 'statistics, VisvaBharati, university, education, research, data analysis, vbstat';
+$author = 'Department of Statistics, VisvaBharati University'; 
+// $theme_color='';
+
+
+//$og_title     /* if not declared default title will be used */
+$og_description='Explore the Department of Statistics at VisvaBharati University and access lecture notes, gallery, and resources.';   /* if not declared default description will be used */
+$og_image_url='https://vbstat.ml/img/2022/siksha-bhabana-football-tournament-2022.jpg';     /* declare this */
+$og_type='website';      /* declare this */
+
+
+
+
+
+// Include the header
+require_once "$includes_folder/header.php";
+?>
+<div class="max-w-screen-xl px-4  mx-auto md:px-6">
+<section class="py-4">
+    <div class="mx-auto max-w-screen-xl  "><div class='rounded-lg ' style="background-image: url(https://vbstat.ml/img/2021/dept-of-stat-from-mathdept.jpg);
+    background-size: cover;">
+        <div class="  rounded-lg  p-8 md:p-12 mb-8" style='backdrop-filter: brightness(0.8)'>
+            <h1 class="text-white text-3xl md:text-5xl font-extrabold mb-2">Department of Statistics Visva-Bharti</h1>
+            <p class="text-lg font-normal text-white mb-6">Lecture notes & gallery - this page is unofficial and maintained by statistics students of department of statistics VisvaBharati University.</p>
+            <a href="/about" class="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                Read more
+                <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </a>
+        </div>
+        </div>
+
+
+        <div class="grid md:grid-cols-2 gap-8">
+            <div class="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
+                
+                <h2 class="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">Our Department</h2>
+                <p class="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">The Department of Statistics at Siksha-Bhavana began in 1999 with Undergraduate Programs in Statistics (Major and Minor). A Postgraduate Program followed in 2003, along with a PhD Program...</p>
+                <a href="<?php echo $site_url; ?>/about/our-department" class="text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center">Read more
+                    <svg aria-hidden="true" class="w-4 h-4 ml-2" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
+                    </svg>
+                </a>
+            </div>
+            <div class="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
+                 
+                <h2 class="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">Shantiniketan</h2>
+                <p class="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">A cultural haven at Santiniketan, where Eastern and Western civilizations converge. Explore diverse aspects of truth through multidimensional perspectives...</p>
+                <a href="<?php echo $site_url; ?>/about/santiniketan" class="text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center">Read more
+                    <svg aria-hidden="true" class="w-4 h-4 ml-2" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
+                    </svg>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section id="links">
+  <div class="py-8 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+    <div class="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
+      <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Important Links</h2>
+      <p class="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">Explore some important pages</p>
+    </div>
+    <div class="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
+
+      <!-- Link 1 -->
+      <div class="items-center bg-gray-50 rounded-lg dark:bg-gray-700 sm:flex dark:bg-gray-800 dark:border-gray-700">
+     <a href="https://onlinebidyacharcha.net/moodle/login/index.php" target="_blank" title="OnlineBidyacharcha our official site for online mediam" rel="nofollow">   <div class="p-4">
+          <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+            OnlineBidyacharcha
+          </h3>
+          <p class="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Log in to Online বিদ্যাচর্চা विद्याचर्चा Bidyacharcha.</p>
+        </div></a>
+      </div>
+
+      <!-- Link 2 -->
+      <div class="items-center bg-gray-50 rounded-lg dark:bg-gray-700 sm:flex dark:bg-gray-800 dark:border-gray-700">
+        <a href="http://vbu.ucanapply.com/resultsheetvbu/" target="_blank"><div class="p-4">
+          <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Result Sheet
+          </h3>
+          <p class="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Result Sheet (Students may view their previous results by changing the session accordingly)</p>
+        </div></a>
+      </div>
+
+      <!-- Link 3 -->
+      <div class="items-center bg-gray-50 rounded-lg dark:bg-gray-700 sm:flex dark:bg-gray-800 dark:border-gray-700">
+       <a href="http://vbu.ucanapply.com/universitysystem/student/" target="_blank" rel="nofollow"> <div class="p-4">
+          <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Exam Section
+          </h3>
+          <p class="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">EXAMINATION SECTION Visva-bharati University.</p>
+        </div></a>
+      </div>
+
+      <!-- Link 4 -->
+      <div class="items-center bg-gray-50 rounded-lg dark:bg-gray-700 sm:flex dark:bg-gray-800 dark:border-gray-700">
+      <a href="http://vbu.ucanapply.com/universitysystem/student/" target="_blank" rel="nofollow">  <div class="p-4">
+          <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Exam Section
+          </h3>
+          <p class="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">EXAMINATION SECTION Visva-bharati University.</p>
+        </div></a>
+      </div>
+
+      <!-- Link 5 -->
+      <div class="items-center bg-gray-50 rounded-lg dark:bg-gray-700 sm:flex dark:bg-gray-800 dark:border-gray-700">
+         <a href="https://drive.google.com/drive/folders/1LUhY6YdcJjOQ-eJTVrJWX1j8GTcm4V3L" target="_blank" rel="nofollow"> <div class="p-4">
+          <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Cricket Tournament
+          </h3>
+          <p class="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Sikha Bhabana Interdepartment Cricket tournament photos.</p>
+        </div></a>
+      </div>
+
+      <!-- Link 6 -->
+      <div class="items-center bg-gray-50 rounded-lg dark:bg-gray-700 sm:flex dark:bg-gray-800 dark:border-gray-700">
+        <a href="https://www.linkedin.com/in/statistics-department-vbu-7ab377266?originalSubdomain=in" target="_blank" rel="nofollow"><div class="p-4">
+          <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Statistics Department LinkedIn
+          </h3>
+          <p class="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Statistics Department VBU
+Placement Coordinator at Visva Bharati, Shantiniketan
+West Bengal, India  </p>
+        </div></a>
+      </div>
+
+    </div>
+  </div>
+</section>
+
+
+
+
+<section class="" id="dept-gallery">
+  <div class="py-8 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+   <div class="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
+      <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Department</h2>
+      <p class="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400"> </p>
+    </div>
+<div class="grid-container">
+<?php
+$directory = "$image_folder/dept"; // Replace with the path to the directory you want to scan
+
+if (is_dir($directory)) {
+    $files = scandir($directory);
+
+    foreach ($files as $file) {
+        if ($file != '.' && $file != '..') {
+            echo '<div><img  class="h-auto max-w-full rounded-lg"  src="'.$directory.'/'.$file.'"  loading="lazy"></div>';
+        }
+    }
+} else {
+    echo "Invalid directory";
+}
+?>
+</div>
+  </div>
+</section>
+
+</div>
+<?php
+// Include the header
+require_once "$includes_folder/footer.php";
+?>
